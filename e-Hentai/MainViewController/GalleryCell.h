350025c8cdc7387209d74aa594da9e6a5142e839
OptimusKe <yeah200077@gmail.com>
2014-09-04 11:53:29 +0800

    新增GalleryCell (使用SDWebImage)

---
+++ e-Hentai/MainViewController/GalleryCell.h
@@ -0,0 +1,13 @@
+//
+//  GalleryCell.h
+//  e-Hentai
+//
+//  Created by Jack on 2014/9/4.
+//  Copyright (c) 2014年 ChilunChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GalleryCell : UICollectionViewCell
+
+@end
---
+++ e-Hentai/MainViewController/GalleryCell.m
@@ -0,0 +1,31 @@
+//
+//  GalleryCell.m
+//  e-Hentai
+//
+//  Created by Jack on 2014/9/4.
+//  Copyright (c) 2014年 ChilunChen. All rights reserved.
+//
+
+#import "GalleryCell.h"
+
+@implementation GalleryCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
