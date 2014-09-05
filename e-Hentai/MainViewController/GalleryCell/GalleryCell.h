1166c06f76ebd28fd5ff040e6b988bdddd04691d
OptimusKe <yeah200077@gmail.com>
2014-09-05 15:06:12 +0800

    add Categoty & Rating to CalleryCell

---
+++ e-Hentai/MainViewController/GalleryCell/GalleryCell.h
@@ -0,0 +1,24 @@
+//
+//  GalleryCell.h
+//  e-Hentai
+//
+//  Created by Jack on 2014/9/4.
+//  Copyright (c) 2014年 ChilunChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CategoryTitle;
+@class RatingStar;
+
+@interface GalleryCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel*       cellLabel;
+@property (weak, nonatomic) IBOutlet UIImageView*   cellImageView;
+@property (weak, nonatomic) IBOutlet CategoryTitle* cellCategory;
+@property (weak, nonatomic) IBOutlet RatingStar*    cellStar;
+
+//設定資料
+-(void)setGalleryDict:(NSDictionary*)dataDict;
+
+@end
