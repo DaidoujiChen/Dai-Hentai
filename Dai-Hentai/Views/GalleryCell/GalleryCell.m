2228d64437c7a1bad749951727647029f0e893b7
DaidoujiChen <daidoujichen@gmail.com>
2017-04-11 18:41:30 +0800

    長按圖片時, 可以對圖片進行動作

--- Dai-Hentai/Views/GalleryCell/GalleryCell.m
+++ Dai-Hentai/Views/GalleryCell/GalleryCell.m
@@ -7,8 +7,46 @@
 //
 
 #import "GalleryCell.h"
+#import "UIAlertController+Block.h"
+
+@interface GalleryCell ()
+
+@property (nonatomic, assign) BOOL isPresenting;
+@property (nonatomic, readonly) UIViewController *rootViewController;
+
+@end
 
 @implementation GalleryCell
+
+#pragma mark - Private Instance Method
+
+- (UIViewController *)rootViewController {
+    return [UIApplication sharedApplication].keyWindow.rootViewController;
+}
+
+- (void)handleLongPress:(UILongPressGestureRecognizer *)longPressGestureRecognizer {
+    if (self.galleryImageView.image && !self.isPresenting) {
+        self.isPresenting = YES;
+        UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:@[ self.galleryImageView.image ] applicationActivities:nil];
+        
+        __weak GalleryCell *weakSelf = self;
+        activityViewController.completionWithItemsHandler = ^(UIActivityType activityType, BOOL completed, NSArray *returnedItems, NSError *activityError) {
+            weakSelf.isPresenting = NO;
+        };
+        [self.rootViewController presentViewController:activityViewController animated:YES completion:nil];
+    }
+}
+
+#pragma mark - Life Cycle
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    UILongPressGestureRecognizer *longPressGestureRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPress:)];
+    longPressGestureRecognizer.delaysTouchesBegan = YES;
+    longPressGestureRecognizer.minimumPressDuration = 1.0f;
+    [self addGestureRecognizer:longPressGestureRecognizer];
+    self.isPresenting = NO;
+    return [super initWithCoder:aDecoder];
+}
 
 - (void)prepareForReuse {
     [super prepareForReuse];
